06a2569e32d19b7a91cdd53a4b29d38efe98a17d
Kai-Xuan <chenkx.jsh@aliyun.com>
2022-03-11 20:06:42 +0800

    update utils practice file

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if __name__ =='__main__':
     parser.add_argument("--norm_type", type=str, default='ln', choices=['bn', 'gn', 'in', 'ln', 'ln2', 'cn','None'])
     parser.add_argument("--pool_type", type=str, default="dke", choices=['dke', 'sum', 'mean', 'max'])
     parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--lr_warmup_type", type=str, default='step', choices=['step','cosine','linear','None'])
+    parser.add_argument("--lr_warmup_type", type=str, default='None', choices=['step','cosine','linear','None'])
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--dropout", type=float, default=0.5)
     parser.add_argument("--weight_decay", type=float, default=1e-4)
--- utils/utils_practice.py
+++ utils/utils_practice.py
@@ -1,7 +1,7 @@
 
 import torch
 
-def batch_trace(batch_tensor):
+def batch_tensor_trace(batch_tensor):
     assert batch_tensor.shape[1] == batch_tensor.shape[2]
     batch_trace = batch_tensor.diagonal(offset=0, dim1=-1, dim2=-2).sum(-1)
     batch_trace = torch.reshape(batch_trace, (batch_trace.shape[0], 1, 1))
